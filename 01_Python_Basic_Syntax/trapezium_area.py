cc4a51ef02ed170d24047c5e537fbd6f75243e25
Codingcooker74 <jjclue74@gmail.com>
2026-01-12 09:00:34 +0900

    Feat: upload

---
+++ 01_Python_Basic_Syntax/trapezium_area.py
@@ -0,0 +1,14 @@
+# trapezium_area.py
+
+def addition(a, b):
+    return a + b
+
+def multiplication(a, b):
+    return a * b
+
+def divided_by_2(x):
+    return x / 2
+
+def trapezium_area(base1, base2, height):
+    # 사다리꼴 넓이 공식: (a + b) * h / 2
+    return (base1 + base2) * height / 2
